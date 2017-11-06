c765d2261c3fc2dae69a3e823ed4f634afd07ca9
Lauri Junkkari <latenssi@gmail.com>
2017-11-06 21:10:09 +0200

    Disable usage of map-settings.json

--- 0.15/docker-entrypoint.sh
+++ 0.15/docker-entrypoint.sh
@@ -28,8 +28,8 @@ fi
 if ! find -L $SAVES -iname \*.zip -mindepth 1 -print | grep -q .; then
   /opt/factorio/bin/x64/factorio \
     --create $SAVES/_autosave1.zip  \
-    --map-gen-settings $CONFIG/map-gen-settings.json \
-    --map-settings $CONFIG/map-settings.json
+    --map-gen-settings $CONFIG/map-gen-settings.json
+    # --map-settings $CONFIG/map-settings.json
 fi
 
 exec /opt/factorio/bin/x64/factorio \
